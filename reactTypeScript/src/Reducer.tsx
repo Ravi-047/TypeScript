64aabde00280361c870ce7f8a6ed03e31748c1b4
Ravi-047 <ravi047raj@gmail.com>
2024-01-03 18:56:08 +0530

    useReducer

---
+++ reactTypeScript/src/Reducer.tsx
@@ -0,0 +1,38 @@
+import { useReducer } from "react"
+
+type State = {
+    count: number
+}
+
+type Action = {
+    type: "INCREMENT",
+    increaseBy: number,
+} | {
+    type: "DECREMENT",
+    decreaseBy: number,
+}
+
+function reducer(state:State, action:Action) {
+    switch (action.type) {
+        case 'INCREMENT':
+            return {...state, count: state.count + action.increaseBy}
+        case 'DECREMENT':
+            return {...state, count: state.count - action.decreaseBy}
+        default:
+            throw new Error()
+    }
+}
+
+const Reducer = () => {
+    const [state, dispatch] = useReducer(reducer, {count: 0})
+  return (
+    <div>
+        <button onClick={() => dispatch({type:"DECREMENT", decreaseBy:1})}> - </button>
+        <span>{state.count}</span>
+        <button onClick={() => dispatch({type:"INCREMENT", increaseBy:1})}> + </button>
+
+    </div>
+  )
+}
+
+export default Reducer
